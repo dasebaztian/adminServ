5364bfb5f952e11c2295df2f1e9f47eddf075d83
Sebastián Sánchez <111941668+dasebaztian@users.noreply.github.com>
2023-08-31 09:36:22 -0600

    Update crearScript.sh

--- crearScript.sh
+++ crearScript.sh
@@ -4,8 +4,8 @@
 
 #Script que crea scripts
 #El primer parametro será el nombre del archivo
-echo "#!/bin/bash" >> $1
-echo "#$USER" >> $1
-date >> $1
-
-nano $1
+echo "#!/bin/bash" >> /home/$USER/bin/$1
+echo "#$USER" >> /home/$USER/bin/$1
+date >> /home/$USER/bin/$1
+chmod 755 /home/$USER/bin/$1
+nano /home/$USER/bin/$1
