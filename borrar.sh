ee1b74a315472483092a4ab4956ba22fdd6592fa
Daniel Sebastian <danielsebastiansanchezm@gmail.com>
2023-09-02 13:19:01 -0600

    Borrar y recuperar fueron mejorados

--- borrar.sh
+++ borrar.sh
@@ -3,4 +3,5 @@
 #sunix  31/Ago/2023
 #Script que disque borra archivos
 # El archivo que se borra es el primer parametro que se le pasa al script
-mv $1 ./.$1
+mkdir -p /home/$USER/.basurero
+mv $1 /home/$USER/.basurero/.$1
--- recuperar.sh
+++ recuperar.sh
@@ -2,5 +2,5 @@
 
 # sunix 31/Ago/2023
 # Recupera un archivo disque eliminado, el archivo es el primer parametro que se le pasa al script
-mv .$1 ./$1
+mv /home/$USER/.basurero/.$1 ./$1
 
